a9ca0bcd023a7fa310373afdb8dc3f23ee1c63b7
Ubuntu <pierre.pericard@gmail.com>
2020-04-26 15:33:10 +0000

    Update

--- source/ovgraphbuild.cpp
+++ source/ovgraphbuild.cpp
@@ -65,21 +65,150 @@ void print_debug(std::string const & read_id,
     debug_stream << "ref offset:  " << ref_offset << std::endl;
     debug_stream << "cigar:       " << align_cigar << std::endl;
     debug_stream << "read seq:    " << read_seq << std::endl;
+    // debug_stream << "revcomp seq: " << (read_seq | views::complement | std::views::reverse | views::to<std::vector>) << std::endl;
     debug_stream << "read offset: " << read_offset << std::endl;
     debug_stream << "#################" << std::endl;
 }
 
+std::int32_t get_last_base_ref_pos(std::int32_t const begin_ref_offset, 
+                                   std::vector<cigar> const & read_cigar)
+{
+    std::int32_t end_ref_offset{begin_ref_offset};
+
+    for (auto & cigar_op : read_cigar)
+    {
+        if (cigar_op == 'M'_cigar_op or cigar_op == 'D'_cigar_op) end_ref_offset += get<0>(cigar_op);
+    }
+
+    return end_ref_offset - 1;
+}
+
+std::int32_t find_alignment_begin_position_on_read(std::vector<cigar> const & read_cigar)
+{
+        // Checks if the given index in the cigar vector is a soft clip.
+        auto soft_clipping_at = [&] (size_t const index) { return read_cigar[index] == 'S'_cigar_op; };
+        // Checks if the given index in the cigar vector is a hard clip.
+        auto hard_clipping_at = [&] (size_t const index) { return read_cigar[index] == 'H'_cigar_op; };
+        // Checks if the given cigar vector as at least min_size many elements.
+        auto vector_size_at_least = [&] (size_t const min_size) { return read_cigar.size() >= min_size; };
+        // Returns the cigar count of the ith cigar element in the given cigar vector.
+        auto cigar_count_at = [&] (size_t const index) { return get<0>(read_cigar[index]); };
+
+        // check for soft clipping at the first two positions
+        if (vector_size_at_least(1) && soft_clipping_at(0))
+            return cigar_count_at(0);
+        else if (vector_size_at_least(2) && hard_clipping_at(0) && soft_clipping_at(1))
+            return cigar_count_at(1);
+}
+
+std::int32_t find_overlap_begin_position_on_read(std::int32_t const read_i_ref_offset,
+                                                 std::int32_t const read_j_ref_offset,
+                                                 std::vector<cigar> const & read_i_cigar)
+{
+    std::int32_t read_i_current_pos_on_read{0};
+    std::int32_t read_i_current_pos_on_ref{read_i_ref_offset};
+
+    for (auto & cigar_op : read_i_cigar)
+    {
+        std::int32_t operation_count = get<0>(cigar_op);
+
+        if (cigar_op == 'S'_cigar_op or cigar_op == 'I'_cigar_op)
+        {
+            read_i_current_pos_on_read += operation_count;
+        }
+        else if (cigar_op == 'D'_cigar_op) // DANGER ZONE
+        {
+            read_i_current_pos_on_ref += operation_count;
+
+            if (read_i_current_pos_on_ref >= read_j_ref_offset) break;
+        }
+        else if (cigar_op == 'M'_cigar_op)
+        {
+            std::int32_t offset = std::min(operation_count, read_j_ref_offset - read_i_current_pos_on_ref);
+            read_i_current_pos_on_read += offset;
+            read_i_current_pos_on_ref += operation_count;
+        }
+
+        if (read_i_current_pos_on_ref >= read_j_ref_offset) break;
+    }
+
+    return read_i_current_pos_on_read;
+}
+
+std::tuple<int32_t, int32_t> find_anchor(std::vector<cigar> const & read_i_cigar,
+                                         std::int32_t const read_i_ref_offset,
+                                         std::vector<cigar> const & read_j_cigar,
+                                         std::int32_t const read_j_ref_offset)
+{
+    std::int32_t read_j_alignment_begin_position_on_read = find_alignment_begin_position_on_read(read_j_cigar);
+    std::int32_t read_i_overlap_begin_position_on_read = find_overlap_begin_position_on_read(read_i_ref_offset, read_j_ref_offset, read_i_cigar);
+
+    return std::make_tuple(read_i_overlap_begin_position_on_read, read_j_alignment_begin_position_on_read);
+}
+
+bool perfect_overlap(std::vector<dna5> const & read_i_seq, 
+                     std::vector<dna5> const & read_j_seq,
+                     std::tuple<int32_t, int32_t> const anchor_positions,
+                     std::uint16_t const min_overlap_length)
+{
+    auto & read_i_anchor_pos = std::get<0>(anchor_positions);
+    auto & read_j_anchor_pos = std::get<1>(anchor_positions);
+
+    std::int32_t upstream_overlap_length = std::min(read_i_anchor_pos, read_j_anchor_pos);
+    std::int32_t downstream_overlap_length = std::min(read_i_seq.size() - read_i_anchor_pos,
+                                                      read_j_seq.size() - read_j_anchor_pos);
+
+    if (upstream_overlap_length + downstream_overlap_length < min_overlap_length) return false;
+
+    auto read_i_overlap_subseq = read_i_seq | views::slice(read_i_anchor_pos - upstream_overlap_length, read_i_anchor_pos + downstream_overlap_length);
+    auto read_j_overlap_subseq = read_j_seq | views::slice(read_j_anchor_pos - upstream_overlap_length, read_j_anchor_pos + downstream_overlap_length);
+
+    // debug_stream << read_i_overlap_subseq << std::endl;
+    // debug_stream << read_j_overlap_subseq << std::endl;
+    // debug_stream << std::endl;
+
+    return std::ranges::equal(read_i_overlap_subseq, read_j_overlap_subseq);
+}
+
+bool are_compatible_reads(std::vector<dna5> const & read_i_seq, 
+                          std::vector<dna5> const & read_j_seq,
+                          std::tuple<int32_t, int32_t> const anchor_positions,
+                          std::uint16_t const min_overlap_length)
+{
+    // Test for perfect identity of the overlap between the 2 reads
+    if (perfect_overlap(read_i_seq, read_j_seq, anchor_positions, min_overlap_length)) return true;
+
+    // If we also have the end pos of the alignment on the reads we can also check
+    // for length difference to quickly find non compatible reads, even when allowing errors
+
+
+    // Additionally, we can perform 2 anchored banded alignment (up and downstream the anchor)
+    // to check for overlaps with errors
+
+
+    return false;
+}
+
+void write_compatible_reads(std::string const & read_i_id,
+                            std::string const & read_j_id,
+                            std::filesystem::path const & output_basepath)
+{
+    std::cout << read_i_id << "\t" << read_j_id << std::endl;
+}
+
 void find_reads_overlaps(single_ref_align_storage_t const & align_storage,
                          std::filesystem::path const & output_basepath, 
-                         uint16_t const min_overlap_length)
+                         std::uint16_t const min_overlap_length)
 {
-    debug_stream << std::endl << std::endl;
-    debug_stream << "Ref idx:  " << align_storage.ref_idx << std::endl;
-    debug_stream << "Read names:  " << std::endl;
-    debug_stream << align_storage.read_ids << std::endl;
-    debug_stream << "#################" << std::endl << std::endl;
+    // debug_stream << std::endl << std::endl;
+    // debug_stream << "Ref idx:  " << align_storage.ref_idx << std::endl;
+    // debug_stream << "Read names:  " << std::endl;
+    // debug_stream << align_storage.read_ids << std::endl;
+    // debug_stream << "#################" << std::endl << std::endl;
 
-    size_t reads_number = align_storage.read_ids.size();
+    std::size_t max_comparison_nb = 1000000000;
+
+    std::size_t reads_number = align_storage.read_ids.size();
 
     if(reads_number>1)
     for(size_t i = 0; i < reads_number-1; ++i)
@@ -89,6 +218,15 @@ void find_reads_overlaps(single_ref_align_storage_t const & align_storage,
         auto & read_i_cigar = align_storage.align_cigars[i];
         auto & read_i_ref_offset = align_storage.ref_offsets[i];
 
+        std::int32_t read_i_last_base_ref_pos = get_last_base_ref_pos(read_i_ref_offset, read_i_cigar);
+
+        // debug_stream << std::endl;
+        // debug_stream << "ref offset:  " << read_i_ref_offset << std::endl;
+        // debug_stream << "cigar:       " << read_i_cigar << std::endl;
+        // debug_stream << "last base:   " << read_i_last_base_ref_pos << std::endl;
+
+        std::int32_t forward_comparison_count{0};
+
         for(size_t j = i+1; j < reads_number; ++j)
         {
             auto & read_j_id = align_storage.read_ids[j];
@@ -96,6 +234,23 @@ void find_reads_overlaps(single_ref_align_storage_t const & align_storage,
             auto & read_j_cigar = align_storage.align_cigars[j];
             auto & read_j_ref_offset = align_storage.ref_offsets[j];
 
+            // Stop comparisons when there is no overlap left possible
+            if (read_j_ref_offset > read_i_last_base_ref_pos) break;
+
+            // Find anchor positions on reads when we know it exists an overlap of size >= 1
+            std::tuple<int32_t, int32_t> anchor_positions = find_anchor(read_i_cigar,
+                                                                        read_i_ref_offset,
+                                                                        read_j_cigar,
+                                                                        read_j_ref_offset);
+
+            bool are_compatible = are_compatible_reads(read_i_seq, read_j_seq, anchor_positions, min_overlap_length);
+
+            if (are_compatible) write_compatible_reads(read_i_id, read_j_id, output_basepath);
+
+            // Limit the number of forward comparisons per read
+            // This is very powerful in limiting search complexity for highly covered references
+            // we go from quadratic O(n²) to amortised linear O(n*max_comparison_nb)
+            if (++forward_comparison_count >= max_comparison_nb) break;
         }
     }
 }
@@ -113,8 +268,11 @@ void clear_align_storage(single_ref_align_storage_t & align_storage)
 void process_alignment_file(std::filesystem::path const & input_path,
                             reference_storage_t const & ref_storage,
                             std::filesystem::path const & output_basepath, 
-                            uint16_t const min_overlap_length)
+                            std::uint16_t const min_overlap_length)
 {
+    debug_stream << "INFO: processing alignment file" << std::endl;
+    time_t start_time = time(NULL);
+
     alignment_file_input input_file{input_path,
                                     ref_storage.ids,
                                     ref_storage.seqs,
@@ -123,13 +281,12 @@ void process_alignment_file(std::filesystem::path const & input_path,
                                            field::REF_ID,
                                            field::REF_OFFSET,
                                            field::CIGAR,
-                                           field::ALIGNMENT,
                                            field::SEQ,
                                            field::OFFSET>{}};
     
     single_ref_align_storage_t align_storage{};
 
-    for (auto & [read_id, flag, ref_idx, ref_offset, align_cigar, alignment, read_seq, read_offset] : input_file)
+    for (auto & [read_id, flag, ref_idx, ref_offset, align_cigar, read_seq, read_offset] : input_file)
     {
         // debug_stream << "Stored ref_idx: " << align_storage.ref_idx << std::endl;
 
@@ -151,7 +308,6 @@ void process_alignment_file(std::filesystem::path const & input_path,
             }
 
             // print_debug(read_id, flag, ref_idx, ref_storage, ref_offset, align_cigar, read_seq, read_offset);
-            debug_stream << alignment << std::endl;
 
             // If read was reverse-complemented before mapping, then revcomp its sequence
             if ((flag & 0x10) == 0x10) read_seq = read_seq | views::complement | std::views::reverse | views::to<std::vector>;
@@ -159,7 +315,7 @@ void process_alignment_file(std::filesystem::path const & input_path,
             // debug_stream << "final seq:   " << read_seq << std::endl;
 
             align_storage.read_ids.push_back(std::move(read_id));
-            align_storage.read_seqs.push_back(std::move(read_seq));
+            align_storage.read_seqs.push_back(std::move(read_seq)); // read_seq is already oriented as per the alignment strand
             align_storage.flags.push_back(std::move(flag));
             align_storage.align_cigars.push_back(std::move(align_cigar));
             align_storage.ref_offsets.push_back(ref_offset.value());
@@ -167,16 +323,19 @@ void process_alignment_file(std::filesystem::path const & input_path,
         }
     }
 
-    if (align_storage.ref_idx >= 0)
+    if (align_storage.ref_idx >= 0 and align_storage.read_ids.size() > 1)
     {
         find_reads_overlaps(align_storage, output_basepath, min_overlap_length);
     }
+
+    debug_stream << "INFO: finished processing alignment file " 
+                 << "in " << time(NULL)-start_time << " seconds" << std::endl;
 }
 
 void run_program(std::filesystem::path const & input_path, 
                  std::filesystem::path const & reference_path,
                  std::filesystem::path const & output_basepath, 
-                 uint16_t const min_overlap_length)
+                 std::uint16_t const min_overlap_length)
 {
     // Read the reference file
     reference_storage_t ref_storage{};
